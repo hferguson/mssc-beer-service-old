2a749136088ad1e57c1241e476f97b5ffcfe1a18
Hugh Ferguson <hugh@hferguson.ca>
2023-10-21 18:08:56 -0400

    Added DTO and pageable list

---
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
@@ -0,0 +1,25 @@
+package guru.springframework.msscbeerservice.web.model;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto {
+    private UUID id;
+    private Integer version;
+
+    private OffsetDateTime createdDate;
+    private OffsetDateTime lastModifiedDate;
+    private String beerName;
+    private BeerStyleEnum beerStyle;
+    private BigDecimal price;
+    private Integer quantityOnHand;
+    private Long upc;
+
+}
